c482ed7552601be5e66bdcd6d7cd41a0d3c49da8
Q <121088532+q-i-n-g@users.noreply.github.com>
2024-04-28 00:48:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,18 @@
 import load_data
 import model
-import matplotlib.pyplot as plt
-import json
 
-model_path = './output/final_model_2/model.npy'
-loss_path = './output/final_model_2/loss.json'
-accuracy_path = './output/final_model_2/val_acc.json'
+model_path = './output/final_model/'
+
+model_path = model_path + 'model.npy'
+
 
 X_test, y_test = load_data.load_mnist('./data', kind='t10k')
 final_model = model.load_model(model_path)
-with open(loss_path, 'r') as f:
-    loss = json.load(f)
-with open(accuracy_path, 'r') as f:
-    accuracy = json.load(f)
 
-img = plt.figure(1)
-x = range(len(loss)*10)[::10]
-plt.plot(x, loss)
-plt.xlabel('steps')
-plt.ylabel('train loss')
-plt.savefig('./output/figure/train_loss_2.png')
-
-img = plt.figure(2)
-x = range(len(accuracy)*1000)[::1000]
-plt.plot(x, accuracy)
-plt.xlabel('steps')
-plt.ylabel('valid accuracy')
-plt.savefig('./output/figure/test_accuracy_2.png')
+X_train, y_train = load_data.load_mnist('./data', kind='train')
+X_valid, y_valid = X_train[:10000], y_train[:10000]
+X_train, y_train = X_train[10000:], y_train[10000:]
 
 print('test accuracy:', final_model.test(X_test, y_test))
+print('train accuracy:', final_model.test(X_train, y_train))
+print('valid accuracy:', final_model.test(X_valid, y_valid))
